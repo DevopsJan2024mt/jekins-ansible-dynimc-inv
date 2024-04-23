b48e082ed512e5f2001d77e992abb5d544b2d657
amarderridj <75400685+amarderridj@users.noreply.github.com>
2024-04-22 23:31:24 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,9 +1,9 @@
 provider "AWS" {
   region ="us-east-1"
 }
-resource "AWS_instance" "AWSEC2"{
- ami="ami-04e5276ebb8451442"
- instance_type="t2.micro"
+resource "aws_instance" "AWSEC2"{
+ ami ="ami-04e5276ebb8451442"
+ instance_type = "t2.micro"
  key_name="DevopsNVirginia"
  security_groups=["launch-wizard-2"]
  tags={
